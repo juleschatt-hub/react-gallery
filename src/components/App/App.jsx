5d79435fd61f5d6f565e9ec6c32a703d5270c710
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-06-21 23:13:07 -0500

    working branch test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,8 +5,8 @@ function App() {
         <header>
           <h1>React Gallery</h1>
         </header>
-
-        <p>The gallery goes here!</p>
+      {/* testing brancgh stuff */}
+        <p>The gallery goes here</p>
         <img src="images/goat_small.jpg"/>
         <img src="images/goat_stache.png"/>
       </div>
